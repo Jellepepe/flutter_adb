f889163ba5f01817ba1f14fc6bc406c896fb7e12
Jellepepe <pepetiebosch@gmail.com>
2024-03-09 01:15:07 +0100

    Implement convience single command function

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_adb/adb_connection.dart';
 import 'package:flutter_adb/adb_crypto.dart';
 import 'package:flutter_adb/adb_stream.dart';
+import 'package:flutter_adb/flutter_adb.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final StateProvider<AdbConnection> adbConnectionProvider = StateProvider((ref) {
@@ -55,115 +56,123 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: StreamBuilder(
-          stream: ref.read(adbConnectionProvider).onConnectionChanged,
-          initialData: false,
-          builder: (context, snapshot) {
-            debugPrint('Connection changed: ${snapshot.data}');
-            if (snapshot.hasData) {
-              if (snapshot.data ?? false) {
-                return Text(
-                  'ADB Flutter Example, Connected to: ${ref.watch(adbConnectionProvider).ip}:${ref.watch(adbConnectionProvider).port}',
-                  style: const TextStyle(fontSize: 32),
-                );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: StreamBuilder(
+            stream: ref.read(adbConnectionProvider).onConnectionChanged,
+            initialData: false,
+            builder: (context, snapshot) {
+              debugPrint('Connection changed: ${snapshot.data}');
+              if (snapshot.hasData) {
+                if (snapshot.data ?? false) {
+                  return Text(
+                    'ADB Flutter Example, Connected to: ${ref.watch(adbConnectionProvider).ip}:${ref.watch(adbConnectionProvider).port}',
+                    style: const TextStyle(fontSize: 32),
+                  );
+                } else {
+                  return const Text(
+                    'ADB Flutter Example, Not connected',
+                    style: TextStyle(fontSize: 32),
+                  );
+                }
               } else {
-                return const Text(
-                  'ADB Flutter Example, Not connected',
-                  style: TextStyle(fontSize: 32),
-                );
+                return const CircularProgressIndicator();
               }
-            } else {
-              return const CircularProgressIndicator();
-            }
+            },
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    IntrinsicWidth(
+                      child: TextFormField(
+                        controller: _ipController,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: ref.watch(adbConnectionProvider).ip,
+                        ),
+                        //validates valid ipv4 address
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Invalid IP';
+                          }
+                          if (!RegExp(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$').hasMatch(value)) {
+                            return 'Invalid IP';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    IntrinsicWidth(
+                      child: TextFormField(
+                        controller: _portController,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: ref.watch(adbConnectionProvider).port.toString(),
+                        ),
+                        // Validates valid port
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Invalid Port';
+                          }
+                          if (int.tryParse(value) == null || int.tryParse(value)! < 0 || int.tryParse(value)! > 65535) {
+                            return 'Invalid Port';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await ref.read(adbConnectionProvider).disconnect();
+                          ref.read(adbConnectionProvider.notifier).state = AdbConnection(
+                            _ipController.text,
+                            int.parse(_portController.text),
+                            AdbCrypto(),
+                          );
+                        }
+                      },
+                      child: const Text('Connect'),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 700),
+                  child: ref.watch(adbStreamProvider).maybeWhen(
+                      data: (adbStream) {
+                        return AdbTerminal(stream: adbStream);
+                      },
+                      loading: () => const CircularProgressIndicator(),
+                      error: (error, stack) {
+                        return Text('Error: $error');
+                      },
+                      orElse: () => const SizedBox()),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final result = await Adb.sendSingleCommand(
+                'monkey -p com.google.android.googlequicksearchbox 1;sleep 3;input keyevent KEYCODE_HOME');
+            debugPrint('Result: $result');
           },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Spacer(),
-                  IntrinsicWidth(
-                    child: TextFormField(
-                      controller: _ipController,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: ref.watch(adbConnectionProvider).ip,
-                      ),
-                      //validates valid ipv4 address
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Invalid IP';
-                        }
-                        if (!RegExp(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$').hasMatch(value)) {
-                          return 'Invalid IP';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  IntrinsicWidth(
-                    child: TextFormField(
-                      controller: _portController,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        hintText: ref.watch(adbConnectionProvider).port.toString(),
-                      ),
-                      // Validates valid port
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Invalid Port';
-                        }
-                        if (int.tryParse(value) == null || int.tryParse(value)! < 0 || int.tryParse(value)! > 65535) {
-                          return 'Invalid Port';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        await ref.read(adbConnectionProvider).disconnect();
-                        ref.read(adbConnectionProvider.notifier).state = AdbConnection(
-                          _ipController.text,
-                          int.parse(_portController.text),
-                          AdbCrypto(),
-                        );
-                      }
-                    },
-                    child: const Text('Connect'),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 700),
-                child: ref.watch(adbStreamProvider).maybeWhen(
-                    data: (adbStream) {
-                      return AdbTerminal(stream: adbStream);
-                    },
-                    loading: () => const CircularProgressIndicator(),
-                    error: (error, stack) {
-                      return Text('Error: $error');
-                    },
-                    orElse: () => const SizedBox()),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          tooltip: 'Send single command',
+          child: const Icon(Icons.send),
+        ));
   }
 }
--- lib/flutter_adb.dart
+++ lib/flutter_adb.dart
@@ -1,7 +1,41 @@
 library flutter_adb;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_adb/adb_connection.dart';
+import 'package:flutter_adb/adb_crypto.dart';
+import 'package:flutter_adb/adb_stream.dart';
+
+class Adb {
+  /// Convenience method to open an ADB connection, open a shell and send a single command, then closing the connection.
+  /// The function will return the sanitized output of the command.
+  static Future<String> sendSingleCommand(
+    String command, {
+    String ip = '127.0.0.1',
+    int port = 5555,
+    AdbCrypto? crypto,
+  }) async {
+    AdbConnection connection = AdbConnection(ip, port, crypto ?? AdbCrypto());
+    if (!await connection.connect()) {
+      debugPrint('Failed to connect to $ip:$port');
+      return '';
+    }
+    String finalCommand = '$command;exit\n';
+    AdbStream stream = await connection.openShell();
+    stream.writeString(finalCommand);
+    String output = await stream.onPayload.fold('', (previous, element) => previous + utf8.decode(element)).timeout(
+      const Duration(minutes: 1),
+      onTimeout: () {
+        debugPrint('Timeout closing the stream.');
+        stream.close();
+        return '';
+      },
+    );
+    await connection.disconnect();
+    // Sanitize output
+    output = output.replaceAll('\r', '');
+    output.endsWith('\n') ? output = output.substring(0, output.length - 1) : output;
+    return output.split(finalCommand).last.trim();
+  }
 }
